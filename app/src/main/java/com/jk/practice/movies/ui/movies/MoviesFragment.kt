ce6103c569b72ff9efa4c68061a4d4459d29f6c5
Juan Carlos Martinez Orjuela <jmartinez@merqueo.com>
2019-11-05 22:32:13 -0500

    [Progress] Define navigation
    - Add animation when change fragment

--- app/src/main/java/com/jk/practice/movies/ui/movies/MoviesFragment.kt
+++ app/src/main/java/com/jk/practice/movies/ui/movies/MoviesFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 import com.jk.practice.movies.R
+import kotlinx.android.synthetic.main.fragment_movies.*
 
 /**
  * A simple [Fragment] subclass.
