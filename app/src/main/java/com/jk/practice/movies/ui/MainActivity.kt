78232c4806fc3989c50129497deaaaaf0a56ddfb
Juan Carlos Martinez Orjuela <jmartinez@merqueo.com>
2019-11-05 22:26:31 -0500

    [Progress] Define navigation
    - configure nav host and show the initial fragment

--- app/src/main/java/com/jk/practice/movies/ui/MainActivity.kt
+++ app/src/main/java/com/jk/practice/movies/ui/MainActivity.kt
@@ -2,12 +2,75 @@ package com.jk.practice.movies.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.*
 import com.jk.practice.movies.R
+import com.jk.practice.movies.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var binding: ActivityMainBinding
+    lateinit var host: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setSupportActionBar(binding.toolbar)
+
+        host = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
+            ?: return
+        val navController = host.navController
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.movies),
+            null
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.bottomNavView.setupWithNavController(navController)
+
+        setOnDestinationChangedListener()
+
+    }
+
+    private fun setOnDestinationChangedListener() {
+
+        host.navController.addOnDestinationChangedListener { _, destination, _ ->
+
+            when (destination.id) {
+                R.id.movieDetail -> {
+                    binding.bottomNavView.visibility = View.GONE
+                    binding.toolbar.visibility = View.VISIBLE
+                }
+                else -> {
+                    binding.bottomNavView.visibility = View.VISIBLE
+                    binding.toolbar.visibility = View.VISIBLE
+                }
+            }
+
+        }
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return host.navController.navigateUp(appBarConfiguration)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val retValue = super.onCreateOptionsMenu(menu)
+        return retValue
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(host.navController)
+                || super.onOptionsItemSelected(item)
     }
 }
